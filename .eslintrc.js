2a878059f4a37f122e9ebff2d480d4115bbafdff
yuzf <yuzfds@163.com>
2021-08-29 15:54:03 +0800

    feat: 🎸 修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
+    "@typescript-eslint/no-var-requires": 0,
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
